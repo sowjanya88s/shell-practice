48795f41102164039c5b5f518e9d7df51be8872d
sowjanya <devopssowjanya88s@gmail.com>
2026-02-25 20:24:58 +0530

    variables program

--- install.sh
+++ install.sh
@@ -2,3 +2,15 @@
 
 user_id=$(id -u)
 echo "user id is: $user_id"
+
+if [ $user_id != 0 ]
+then
+echo " pls run this with root user privileges"
+dnf install mysql -y
+if [ $? != 0 ];
+then
+echo "mysql installation ... success"
+exit 1
+else
+echo "mysql installation ... failed"
+fi
