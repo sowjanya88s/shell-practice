026860e47f2225261fe425f2560cd8769390d528
sowjanya <devopssowjanya88s@gmail.com>
2026-02-25 19:35:38 +0530

    variables program

--- special-variables.sh
+++ special-variables.sh
@@ -2,9 +2,9 @@
 
 echo "args passed: $@"
 echo "no.of arguments passed: $#"
-echo "current username is: $user"
-echo "present working directory is: $pwd"
-echo "home directory of current user is: $home"
+echo "current username is: $USER"
+echo "present working directory is: $PWD"
+echo "home directory of current user is: $HOME"
 echo "script name is: $0"
 echo "process id: $$"
 sleep 10 &
