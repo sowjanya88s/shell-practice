eb8d4bbbef8ecdd71b9444fe04dda0c03076959e
sowjanya <devopssowjanya88s@gmail.com>
2026-02-25 20:09:46 +0530

    variables program

--- operators.sh
+++ operators.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 number=$1
-if [ $1 -gt 25 ];
+if [ $number -gt 25 ];
 then
-echo "given number $1 is greater than 25"
-elif [ $1 -lt 25];
-echo "given number $1 is less than 25"
+echo "given number: $number is greater than 25"
+elif [ $number -lt 25 ];
+echo "given number: $number is less than 25"
 else
-echo "given number $1 is equal to 25"
+echo "given number: $number is equal to 25"
 fi
