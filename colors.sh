5aa6a19953e5c0b0c4d6e0f6e639c7ffb4da46b5
sowjanya <devopssowjanya88s@gmail.com>
2026-02-26 20:07:04 +0530

    changes

---
+++ colors.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+LOG_DIR=/var/log/shell-script
+LOG_file=/var/log/shell-script/$0.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+user_id=$(id -u)
+echo "user id is: $user_id"
+
+if [ $user_id -ne 0 ]; then
+echo " $R pls run this with root user privileges $N" | tee -a $LOG_file
+exit 1
+fi
+
+mkdir -p $LOG_DIR
+
+validate() {
+    if [ $1 -eq 0 ] ; then
+    echo " $G $2 ... success $N" | tee -a $LOG_file
+else 
+    echo " $R $2 ... failure $N" | tee -a $LOG_file
+    exit 1
+    fi
+}
+
+for package in $@
+do
+    yum list installed | grep $package &>> $LOG_file
+    if [ $? -ne 1 ] ; then
+    echo " $Y $package is already installed $N " 
+    else
+    yum install $package -y &>> $LOG_file
+    validate "$?" "installation of $package"
+    fi
+done
+
+
