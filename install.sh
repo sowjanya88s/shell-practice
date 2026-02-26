f689885fa33c3591cbd58d2bb1a1554c2fecfcad
sowjanya <devopssowjanya88s@gmail.com>
2026-02-26 19:09:42 +0530

    changes

--- install.sh
+++ install.sh
@@ -10,8 +10,24 @@ fi
 
 sudo dnf install nginx -y
 if [ $? -ne 1 ]; then
+echo "nginx installation ... success"
+exit 1
+else
+echo "nginx installation ... failed"
+fi
+
+sudo dnf install mysql -y
+if [ $? -ne 1 ]; then
 echo "mysql installation ... success"
 exit 1
 else
 echo "mysql installation ... failed"
+fi
+
+sudo dnf install vsftpd -y
+if [ $? -ne 1 ]; then
+echo " vsftpd installation ... success"
+exit 1
+else
+echo "vsftpd installation ... failed"
 fi
