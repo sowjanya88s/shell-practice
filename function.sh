931625db6bc18186a3e6857ada686ce1afd3d57e
sowjanya <devopssowjanya88s@gmail.com>
2026-02-26 19:23:22 +0530

    changes

---
+++ function.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+user_id=$(id -u)
+echo "user id is: $user_id"
+
+if [ $user_id -ne 0 ]; then
+echo " pls run this with root user privileges"
+exit 1
+fi
+
+validate ( $1 -eq 0 ) {
+    echo "$2 ... success"
+else 
+    echo "$2 ... failure"
+}
+
+sudo dnf install nginx -y
+validate $? "installing nginx"
+
+sudo dnf install mysql -y
+validate $? "installing mysql"
+
+sudo dnf install vsftpd -y
+validate $? "installing vsftpd"
