05b3a3cd01886f7a436a3d1b74040d37d50db62c
sowjanya <devopssowjanya88s@gmail.com>
2026-03-01 17:14:30 +0530

    mod

---
+++ script-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COUNTRY=INDIA
+
+echo "I am in: $COUNTRY"
+echo "PID of SCRIPT-1: $$"
+
+sh script-2.sh
+#source ./script-2.sh
---
+++ script-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+echo "I am in: $COUNTRY"
+echo "PID of SCRIPT-2: $$"
+
