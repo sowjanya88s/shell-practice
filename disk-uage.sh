e06258b61f538cc5b6c2742e81d3f7fd104f3866
sowjanya <devopssowjanya88s@gmail.com>
2026-03-04 20:11:46 +0530

    new

--- disk-uage.sh
+++ disk-uage.sh
@@ -10,8 +10,8 @@ log() {
 Disk_usage=$(df -Th | grep -v Filesystem)
 
 while IFS= read -r line; do
-cpu_utilization=$(echo $line | awk -F  '{print $6}' | cut -d '%' -f1)
-path=$(echo $line | awk -F  '{print $7}')
+cpu_utilization=$(echo $line | awk  '{print $6}' | cut -d '%' -f1)
+path=$(echo $line | awk  '{print $7}')
 usage_Threshold=3
     if [ "$cpu_utilization" -gt "$usage_Threshold" ]; then
         message+= "high disk usage on $path : $cpu_utilization \n"
